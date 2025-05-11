75c74125fdda2c5d2b3b977f4b4ff4fe8238fc03
diryivuze <danieliryivuze4@gmail.com>
2025-05-12 00:15:54 +0200

    solving pycodestyle validation errors

--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -1,18 +1,22 @@
 #!/usr/bin/env python3
-""" From the previous file, import wait_n into 2-measure_runtime.py.
-    Create a measure_time function with integers n and max_delay as arguments
-    that measures the total execution time for wait_n(n, max_delay), and
-    returns total_time / n. Your function should return a float.
-    Use the time module to measure an approximate elapsed time. """
-import asyncio  
+"""
+Measure runtime of asynchronous function
+"""
+
+import asyncio
 import time
+
+
 wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
 def measure_time(n: int, max_delay: int) -> float:
-    """ Measure the runtime """
-    start_time = time.time()
+    """
+    Function that measures the total execution time for 'wait_n(n, max_delay)'
+    Return -> float:
+        'total_time/n'
+    """
+    s = time.perf_counter()
     asyncio.run(wait_n(n, max_delay))
-    end_time = time.time()
-    total_time = end_time - start_time
+    total_time = time.perf_counter() - s
     return total_time / n
